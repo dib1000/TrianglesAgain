24c523ecc2ba1b8ceac6358903f0a8bf229d912b
dib1000 <dib5017@gmail.com>
2020-10-29 17:21:21 -0400

    I tested the Triangles constructors and they work

--- Tester.java
+++ Tester.java
@@ -3,8 +3,14 @@ public class Tester {
     Point p1 = new Point(0.0,2.0);
     Point p2 = new Point(2.0,0.0);
     Point p3 = new Point(0.0,2.0);
+    Point p4 = new Point(2.0,2.0);
     System.out.println(p2.distanceTo(p1));
     System.out.println(p2.equals(p1));
     System.out.println(p1.equals(p3));
+    System.out.println();
+    Triangle t1 = new Triangle(p1,p2,p4);
+    Triangle t2 = new Triangle(0.0,2.0,2.0,0.0,2.0,2.0);
+    System.out.println(t1.toString());
+    System.out.println(t2.toString());
   }
 }
