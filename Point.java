21ba934758bae52260804dded8605adf30cb9dc5
dib1000 <dib5017@gmail.com>
2020-10-29 17:10:19 -0400

    I tested the distanceTo method and it works

---
+++ Point.java
@@ -0,0 +1,28 @@
+public class Point{
+  private double x,y;
+
+  //construct a point given coordinates
+  public Point(double X, double Y){
+    x=X;
+    y=Y;
+  }
+
+  //construct a point given a point  (redundant , just provides flexibility)
+  public Point(Point p){
+    x= p.x;  //private access in the same file is allowed!
+    y= p.y;
+  }
+
+  public double getX(){
+    return x;
+  }
+
+  public double getY(){
+    return y;
+  }
+  public double distanceTo(Point other) {
+    double xdif = x - other.x;
+    double ydif = y - other.y;
+    return Math.sqrt((xdif*xdif)+(ydif*ydif));
+  }
+}
---
+++ Tester.java
@@ -0,0 +1,7 @@
+public class Tester {
+  public static void main(String[] args) {
+    Point p1 = new Point(0.0,2.0);
+    Point p2 = new Point(2.0,0.0);
+    System.out.println(p2.distanceTo(p1));
+  }
+}
